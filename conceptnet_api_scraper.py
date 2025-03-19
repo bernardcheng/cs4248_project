0a37cbb34c7cfb543e3db392b3592ca0e1859ca3
bernardcheng <super.bernard.cheng@gmail.com>
2025-03-19 23:14:48 +0800

    added basic pre-processing

--- conceptnet_api_scraper.py
+++ conceptnet_api_scraper.py
@@ -1,7 +1,25 @@
 import requests
+import pandas as pd
 from utils.text2uri import standardized_uri
 
-def main(file_path:str) -> None:    
+def preprocess(file_path:str) -> list:
+    """
+    Helper function to pre-process and clean input csv file.
+    """
+    df = pd.read_csv(file_path)
+
+    col = df.columns[0]
+    df[col] = df[col].str.lower()
+
+    df = df.drop_duplicates()
+
+    return df[col].to_list()    
+    
+
+def main(file_path:str) -> None:
+ 
+    word_list = preprocess(file_path)
+    
     pass
 
 

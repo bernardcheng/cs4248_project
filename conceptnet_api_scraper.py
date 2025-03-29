c31b0a8c39515baaa314c3dd7f07dc8adb271bcf
bernardcheng <super.bernard.cheng@gmail.com>
2025-03-29 23:56:48 +0800

    wip vectorization

--- conceptnet_api_scraper.py
+++ conceptnet_api_scraper.py
@@ -6,8 +6,9 @@ import pandas as pd
 from tqdm import tqdm
 from utils.text2uri import standardized_uri
 
-INPUT_PATH = os.path.join(os.getcwd(), "data", "gender_neutral")
-OUTPUT_PATH = os.path.join(os.getcwd(), "data", "conceptnet_api")
+KEYWORD_PATH = os.path.join(os.getcwd(), "data", "gender_neutral")
+JSON_PATH = os.path.join(os.getcwd(), "data", "conceptnet_api", "json")
+CSV_PATH = os.path.join(os.getcwd(), "data", "conceptnet_api", "csv")
 
 def preprocess(file_path:str) -> list:
     """
@@ -22,7 +23,10 @@ def preprocess(file_path:str) -> list:
     return df[col_name].to_list()    
     
 
-def main(input_folder:str, output_folder:str, edge_limit:int=500) -> None:
+def scrape_keywords(input_folder:str, output_folder:str, edge_limit:int=500) -> None:
+    """
+    Sends URI keyword request to ConceptNet API Server to obtain and store raw query results in JSON format.
+    """
 
     csv_files = [file for file in os.listdir(input_folder) if file.split('.')[-1]=='csv']
 
@@ -33,7 +37,7 @@ def main(input_folder:str, output_folder:str, edge_limit:int=500) -> None:
         all_words = preprocess(f"{input_folder}/{file}")
         new_words = [word for word in all_words if word not in completed_words]
         elapsed_time = 1.00 # initialise time as 1 second
-        keywords = {'start_label': [], 'end_label': [], 'rel_id': [], 'surface_text': [], 'weight': []}
+        
 
         for word in tqdm(new_words):
 
@@ -42,27 +46,48 @@ def main(input_folder:str, output_folder:str, edge_limit:int=500) -> None:
 
             start_time = time.time()
             std_uri = standardized_uri(language='en', term=word)
-            obj = requests.get(f'http://api.conceptnet.io{std_uri}?offset=0&limit={edge_limit}&language=en').json()            
+            obj = requests.get(f'http://api.conceptnet.io/query?node={std_uri}&other=/c/en&limit={edge_limit}').json()     
 
             with open(f'{output_folder}/{word}.json', "w") as json_file:
-                    json.dump(obj['edges'], json_file, indent=4)
+                    json.dump(obj['edges'], json_file, indent=4)     
 
-            for edge in obj['edges']:
+            elapsed_time = time.time() - start_time 
+    return
 
-                # Extract keyword infromation
-                keywords['start_label'].append(edge['start']['label'])
-                keywords['end_label'].append(edge['end']['label'])
-                keywords['rel_id'].append(edge['rel']['@id'])
-                keywords['surface_text'].append(edge['surfaceText'])
-                keywords['weight'].append(edge['weight'])                
+def parse_response(input_folder:str, output_folder:str) -> None:
+    """
+    Helper function to read raw JSON files and extract key field information from each edge.        
+    """
+    json_files = [file for file in os.listdir(input_folder) if file.split('.')[-1]=='json']
 
-            elapsed_time = time.time() - start_time
-        
-        keywords_df = pd.DataFrame.from_dict(keywords)
-        filename = file.split('.')[0]
-        keywords_df.to_csv(f'{output_folder}/{filename}.csv', index=False)
+    keywords = {'end_id': [], 'end_label': [], 'start_id': [], 'start_label': [], 'rel_id': [], 'surface_text': [], 'weight': [], 'dataset': []}
+
+    for file in tqdm(json_files):
+        with open(f"{input_folder}/{file}", 'r') as f:
+            data = json.load(f)
+
+            # iterate each edge and get average of edge weights
+            weights = [edge['weight'] for edge in data]
+            if len(weights) > 0: 
+                ave_weight = sum(weights)/len(weights)  
+
+                for edge in data:
+                    if edge['weight'] >= ave_weight:
+                        # Store keyword infromation
+                        keywords['end_id'].append(edge['end']['@id'])
+                        keywords['end_label'].append(edge['end']['label'])
+                        keywords['start_id'].append(edge['start']['@id'])
+                        keywords['start_label'].append(edge['start']['label'])                
+                        keywords['rel_id'].append(edge['rel']['@id'])
+                        keywords['surface_text'].append(edge['surfaceText'])
+                        keywords['weight'].append(edge['weight']) 
+                        keywords['dataset'].append(edge['dataset'])   
+
+    keywords_df = pd.DataFrame.from_dict(keywords).drop_duplicates()
+    keywords_df.to_csv(f'{output_folder}/edge_extract.csv', index=False)
     return
 
 
 if __name__ == "__main__":    
-    main(input_folder=INPUT_PATH, output_folder=OUTPUT_PATH)
+    # scrape_keywords(input_folder=KEYWORD_PATH, output_folder=JSON_PATH)
+    parse_response(input_folder=JSON_PATH, output_folder=CSV_PATH)
